c51d50f1243ee9dbbdd59bbe9b4c448392bed3d9
berenzorn <berenzorn@gmail.com>
2020-04-30 12:36:03 +0300

    new list & chain

--- chain.py
+++ chain.py
@@ -1,127 +1,137 @@
-class TwoWayList:
+import hashlib
+import json
+from datetime import datetime, timezone, timedelta
+
+
+class Block:
+
+    def __init__(self, data):
+        self.index = None
+        self.time = None
+        self.data = data
+        self.next = None
+        self.prev = None
+        self.hash = None
+        self.prev_hash = None
+
+    def show(self):
+        print(f"Block: {self.data}")
+
+
+class LinkedList:
 
     def __init__(self):
         self.header = None
         self.tail = None
+        self.index = 0
+
+    def length(self):
+        return self.index
+
+    def get_next_index(self):
+        return self.index + 1
 
     def is_empty(self):
         return False if self.header is not None else True
 
     def append(self, data):
-        block = Chain.Block(data)
+        block = Block(data) if not isinstance(data, Block) else data
         if self.header is None:
             self.header = block
         else:
             self.tail.next = block
             block.prev = self.tail
         self.tail = block
+        self.index += 1
 
-    def show(self, reversed=False):
-        if reversed:
+    def show_all(self, reverse=False):
+        if reverse:
             print("Checking from tail: ")
             current = self.tail
             while current is not None:
-                current.show_block()
+                current.show()
                 current = current.prev
         else:
             print("Checking chain: ")
             current = self.header
             while current is not None:
-                current.show_block()
+                current.show()
                 current = current.next
 
-    def check(self, block):
+    def show_tail(self):
+        current = self.tail
+        current.show()
+
+    def get_tail_hash(self):
+        return self.tail.hash if self.tail is not None else False
+
+    def find(self, data):
+        block = Block(data) if not isinstance(data, Block) else data
         current = self.header
         while current.data != block.data:
             current = current.next
             if current is None:
                 return False
-        return current
-
-
-class Collector:
-
-    def __init__(self):
-        self.array = TwoWayList()
-
-    def append(self, data):
-        self.array.append(data)
-
-    def show(self):
-        self.array.show()
-
-
-class Transaction:
-
-    def __init__(self):
-        self.data = dict()
+        return True
 
 
 class Chain:
 
-    class Block:
-        def __init__(self, data=None):
-            self.data = data
-            self.next = None
-            self.prev = None
-            # self.hash = 0
-            self.prev_hash = 0
-
     def __init__(self):
-        self.chain = TwoWayList()
-        self.current_trns = []
+        self.chain = LinkedList()
+        self.current_trs = LinkedList()
 
-    def new_block(self, data):
-        # block = {
-        #     'index': len(self.chain) + 1,
-        #     'timestamp': time(),
-        #     'transactions': self.current_transactions,
-        #     'proof': proof,
-        #     'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        # }
-        TwoWayList.append(self.chain, data)
-        # self.current_transactions = []
-        #
-        # self.chain.append(block)
-        # return block
+    def add_block(self):
+        block = Block(self.current_trs)
+        self.current_trs = LinkedList()
+        block.index = self.chain.get_next_index()
+        block.time = self.timestamp()
+        block.prev_hash = self.chain.get_tail_hash()
+        block.hash = self.hash(block)
+        self.chain.append(block)
 
-    def new_transaction(self, data):
-        self.current_trns.append(data)
-        # self.current_transactions.append({
-        #     'sender': sender,
-        #     'recipient': recipient,
-        #     'amount': amount,
-        # })
-        #
-        # return self.last_block['index'] + 1
+    def add_transaction(self, data):
+        self.current_trs.append(data)
 
-    @staticmethod
-    def hash(block):
-        # Хеширует блок
-        pass
-
-    @property
     def last_block(self):
-        return self.chain[-1]
+        return self.chain.tail
 
     @staticmethod
     def hash(block):
-        # block_string = json.dumps(block, sort_keys=True).encode()
-        # return hashlib.sha256(block_string).hexdigest()
-        pass
+        # TODO block.data linked list serialization
+        string = json.dumps(block.data).encode()
+        return hashlib.sha256(string).hexdigest()
+
+    @staticmethod
+    def timestamp():
+        epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)  # use POSIX epoch
+        now = datetime.now(timezone.utc)
+        timestamp_micros = (now - epoch) // timedelta(microseconds=1)
+        return timestamp_micros
 
 
 if __name__ == '__main__':
-    chain = TwoWayList()
-    chain.append('1')
-    chain.append('2')
-    chain.append('3')
-    chain.append('4')
-    chain.append('5')
-    chain.show()
-    print()
-    chain.show(reversed=True)
-    print()
-    block3 = Chain.Block('3')
-    if chain.check(block3):
-        print(block3.data, "found")
+    # f = LinkedList()
+    # print(f.is_empty())
+    # f.append(Block('1'))
+    # f.append(Block('2'))
+    # f.append(Block('3'))
+    # f.show_all()
+    # print(f.is_empty())
+    # f.append('5')
+    # f.append('6')
+    # f.append('7')
+    # f.show_all(reverse=True)
+    # print(f.length())
+    # f.show_tail()
+    # print(f.find(Block('3')))
+    # print(f.find('3'))
+    # print(f.find(Block('8')))
+    # print(f.find('8'))
+
+    c = Chain()
+    c.add_transaction('1')
+    c.add_transaction('2')
+    c.add_transaction('3')
+    c.add_block()
+    print(c.last_block().data)
---
+++ linked_list.py
@@ -0,0 +1,83 @@
+class Block:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+    def show(self):
+        print(f"Block: {self.data}")
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.header = None
+        self.tail = None
+        self.index = 0
+
+    def length(self):
+        return self.index
+
+    def is_empty(self):
+        return False if self.header is not None else True
+
+    def append(self, data):
+        block = Block(data) if not isinstance(data, Block) else data
+        if self.header is None:
+            self.header = block
+        else:
+            self.tail.next = block
+            block.prev = self.tail
+        self.tail = block
+        self.index += 1
+
+    def show_all(self, reverse=False):
+        if reverse:
+            print("Checking from tail: ")
+            current = self.tail
+            while current is not None:
+                current.show()
+                current = current.prev
+        else:
+            print("Checking chain: ")
+            current = self.header
+            while current is not None:
+                current.show()
+                current = current.next
+
+    def show_tail(self):
+        current = self.tail
+        current.show()
+
+    def find(self, data):
+        block = Block(data) if not isinstance(data, Block) else data
+        current = self.header
+        while current.data != block.data:
+            current = current.next
+            if current is None:
+                return False
+        return True
+
+
+
+
+if __name__ == '__main__':
+    f = LinkedList()
+    print(f.is_empty())
+    f.append(Block('1'))
+    f.append(Block('2'))
+    f.append(Block('3'))
+    f.show_all()
+    print(f.is_empty())
+    f.append('5')
+    f.append('6')
+    f.append('7')
+    f.show_all(reverse=True)
+    print(f.length())
+    f.show_tail()
+    print(f.find(Block('3')))
+    print(f.find('3'))
+    print(f.find(Block('8')))
+    print(f.find('8'))
+
